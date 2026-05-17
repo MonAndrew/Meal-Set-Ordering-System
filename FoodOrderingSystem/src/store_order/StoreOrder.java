ee65d6e4bb7d9cac9471dc99646ab71b8219f7ee
MonAndrew <ludovicemonandrew@gmail.com>
2026-05-18 05:16:41 +0800

    chore: added StoreOrder

---
+++ FoodOrderingSystem/src/store_order/StoreOrder.java
@@ -0,0 +1,15 @@
+package store_order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreOrder {
+    private List<StoreOrder> storeOrderList;
+
+    public StoreOrder(){
+        this.storeOrderList = new ArrayList<>();
+    }
+
+
+
+}
