c3d9e272133178199aa2db1dad129618a61c57ff
Junwel Conson <junwelconson65@gmail.com>
2026-05-20 22:50:46 +0800

    feat: Update StoreMeals.java
    stored items are going to be save here, extended by the StoreOrder class

--- FoodOrderingSystem/src/store_order/StoreMeals.java
+++ FoodOrderingSystem/src/store_order/StoreMeals.java
@@ -1,34 +1,30 @@
 package store_order;
 
-import java.util.ArrayList;
-import java.util.List;
+public class StoreItem {
 
-public class StoreMeals {
-
-    private List<StoreMeals> storeOrderedMealsList = new ArrayList<>();
-
-    private String foodName;
-    private double foodPrice;
+    private String name;
+    private double price;
     private int quantity;
 
-    public StoreMeals(String foodName, double foodPrice) {
-        this.foodName = foodName;
-        this.foodPrice = foodPrice;
-        this.quantity += 1;
-    }  
-
-    public void addStoreMeals(StoreMeals storeMeal){
-        this.storeOrderedMealsList.add(storeMeal);
+    public StoreItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
 
-    public void checkStoreMeal(List<StoreMeals> list, StoreMeals store){
-        for(StoreMeals l : list){
-            if(l.foodName.equals(store.foodName)){
-                
-            }
-        }
+    public String getName() {
+        return name;
     }
 
-    //TODO add method that store all meals menu and checker for meal dupes to increase quantity
-    
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotal() {
+        return price * quantity;
+    }
 }
