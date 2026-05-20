c74b051fa862be85ebd8fb51ab1e4cd8d8d04c0f
Junwel Conson <junwelconson65@gmail.com>
2026-05-20 22:49:51 +0800

    feat: Update StoreOrder.java
    can store Items as orders

--- FoodOrderingSystem/src/store_order/StoreOrder.java
+++ FoodOrderingSystem/src/store_order/StoreOrder.java
@@ -1,15 +1,55 @@
 package store_order;
 
+/**
+ *
+ * @author Junwel
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 public class StoreOrder {
-    private List<StoreOrder> storeOrderList;
 
-    public StoreOrder(){
-        this.storeOrderList = new ArrayList<>();
+    private List<StoreItem> items = new ArrayList<>();
+
+    public void addItem(StoreItem newItem) {
+
+        for (StoreItem item : items) {
+
+            if (item.getName().equals(newItem.getName())) {
+
+                int newQty = item.getQuantity() + newItem.getQuantity();
+
+                items.remove(item);
+
+                items.add(new StoreItem(
+                        item.getName(),
+                        item.getPrice(),
+                        newQty
+                ));
+
+                return;
+            }
+        }
+
+        items.add(newItem);
     }
 
+    public List<StoreItem> getItems() {
+        return items;
+    }
 
+    public double getTotal() {
 
+        double total = 0;
+
+        for (StoreItem item : items) {
+            total += item.getTotal();
+        }
+
+        return total;
+    }
+
+    public void clear() {
+        items.clear();
+    }
 }
